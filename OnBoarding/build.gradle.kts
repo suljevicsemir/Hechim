902357d005f7994b11228a70af0cd46fad4927f2
Semir Suljevic <suljevic.semir@gmailcom>
2024-08-12 16:04:12 +0200

    feat(onboarding): added feature plugin feature/onboarding

--- OnBoarding/build.gradle.kts
+++ OnBoarding/build.gradle.kts
@@ -1,32 +1,13 @@
+
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.convention.feature)
+    alias(libs.plugins.convention.android.library.compose)
 }
 
 android {
     namespace = "com.semirsuljevic.onboarding"
-    compileSdk = 34
 
-    defaultConfig {
-        minSdk = 26
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
